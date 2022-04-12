381c1512ed7fd70b3df8bef938a946447c37e03a
TheK321 <thek321xd@gmail.com>
2022-04-11 21:45:33 -0500

    Parte del desafío

--- app/src/main/java/com/itz/isc/clientesocket/MainActivity.java
+++ app/src/main/java/com/itz/isc/clientesocket/MainActivity.java
@@ -4,6 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.os.StrictMode;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.io.BufferedReader;
@@ -14,13 +17,22 @@ import java.net.Socket;
 
 public class MainActivity extends AppCompatActivity {
     private TextView output;
+    private EditText etMensaje;
+    private Button btnEnviar;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         if (android.os.Build.VERSION.SDK_INT > 9) { StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build(); StrictMode.setThreadPolicy(policy); }
         output = (TextView) findViewById(R.id.TextViewSalida);
-        ejecutaCliente();
+        etMensaje = findViewById(R.id.etMensaje);
+        btnEnviar = findViewById(R.id.btnEnviar);
+        btnEnviar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ejecutaCliente();
+            }
+        });
     }
 
     private void ejecutaCliente() {
@@ -33,9 +45,9 @@ public class MainActivity extends AppCompatActivity {
                     new InputStreamReader(sk.getInputStream()));
             PrintWriter salida = new PrintWriter(
                     new OutputStreamWriter(sk.getOutputStream()),true);
-            log("enviando...");
-            salida.println("Hola Mundo");
-            log("recibiendo ... " + entrada.readLine());
+            //log("enviando...");
+            salida.println(etMensaje.getText().toString());
+            //log("recibiendo ... " + entrada.readLine());
             sk.close();
         } catch (Exception e) {
             log("error: " + e.toString());
